ec4b960eb643cfe4e0d751660ab7443919b6929b
Becky75 <alpaca.love5@gmail.com>
2022-10-19 16:17:02 -0700

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("Artist").style.fontSize = "30px";
+    } else {
+        document.getElementById("Artist").style.fontSize = "90px";
+    }
+}
